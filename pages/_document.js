b9087bc32bbc3fde3bfac436c912df975f247ff1
yangbin185 <yangbin185@jd.com>
2021-03-31 23:19:25 +0800

    update

--- pages/_document.js
+++ pages/_document.js
@@ -2,6 +2,19 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 import { IS_DEV } from '@/utils'
 
+const baiduHmScript = () => {
+  return {
+    __html: `
+    var _hmt = _hmt || [];
+    (function() {
+      var hm = document.createElement("script");
+      hm.src = "https://hm.baidu.com/hm.js?9598bdf276817c07e9d4393a5bd0da08";
+      var s = document.getElementsByTagName("script")[0];
+      s.parentNode.insertBefore(hm, s);
+    })();`,
+  }
+}
+
 class MyDocument extends Document {
   // static async getInitialProps (ctx) {
   //   const initialProps = await Document.getInitialProps(ctx)
@@ -19,7 +32,7 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
           <script type="text/javascript" src="https://s9.cnzz.com/z_stat.php?id=1279768164&web_id=1279768164"></script>
-          <script type="text/javascript" src="https://hm.baidu.com/hm.js?9598bdf276817c07e9d4393a5bd0da08"></script>
+          <script dangerouslySetInnerHTML={baiduHmScript()} />
         </body>
       </Html>
     )
