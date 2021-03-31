0074832bea503f590bf8c1805574cc50c7997ab2
yangbin185 <yangbin185@jd.com>
2021-03-31 17:10:48 +0800

    update

--- components/link/index.js
+++ components/link/index.js
@@ -1,9 +1,9 @@
 import Link from 'next/link';
 
-const CustomLink = ({ as, href, className, title, children }) => {
+const CustomLink = ({ as, href, className, title, children, openBlank }) => {
   return (
     <Link as={as} href={href}>
-      <a className={className} title={title}>{children}</a>
+      <a className={className} title={title} target={openBlank ? '_blank' : '_self'}>{children}</a>
     </Link>
   )
 }
